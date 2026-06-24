ccccaccd2741991f759d26089b4f11141b9e377e
thagorn <thagorn1@gmail.com>
2026-06-24 05:42:22 -0400

    finish mana + soul UI

--- ui/mana_drop.lua
+++ ui/mana_drop.lua
@@ -13,8 +13,24 @@ local function create_mana_sprite()
     return m_s
 end
 
+local function create_soul_sprite()
+    local s_s = SMODS.create_sprite(
+                    0,
+                    0,
+                    G.CARD_W * 0.5,
+                    G.CARD_H * 0.5,
+                    SMODS.get_atlas("wlt_ui"),
+                    { x = 1, y = 0 }
+                )
+    s_s.states.drag.can = false
+    s_s.states.hover.can = false
+    s_s.states.collide.can = false
+    return s_s
+end
+
 function create_mana_UI()
     local m_s = create_mana_sprite()
+    local s_s = create_soul_sprite()
     G.mana_ui = UIBox{
             definition = 
                 {n=G.UIT.ROOT, config = 
@@ -28,16 +44,17 @@ function create_mana_UI()
                     {
                         {n=G.UIT.R, config = {align = 'cm', maxw = 1}, nodes =
                             {
+                                {n=G.UIT.O, config={id = 'mana_drop', object = m_s}},
                                 {n=G.UIT.B, config={h = 0, w = 0.1}},
-                                {n=G.UIT.O, config={object=m_s}},
-                                {n=G.UIT.B, config={h = 0, w = 0.1}},
+                                {n=G.UIT.O, config={id = 'soul_drop', object = s_s}},
                             }
                         }
                     }
                 },
                 config = {}
             }
-    mana_ui_text = UIBox{
+    -- Mana value text
+    UIBox{
             definition = 
                 {n=G.UIT.ROOT, config= {align = 'cm', colour = G.C.CLEAR}, nodes={
                     {n=G.UIT.R, config = {align = 'cm', maxw = 1}, nodes={
@@ -52,7 +69,26 @@ function create_mana_UI()
                 config = {
                     align = 'cm',
                     offset = { x = 0, y = 0.18 },
-                    major = G.mana_ui
+                    major = G.mana_ui:get_UIE_by_ID('mana_drop')
+                }
+            }
+    -- Soul value text
+    UIBox{
+            definition = 
+                {n=G.UIT.ROOT, config= {align = 'cm', colour = G.C.CLEAR}, nodes={
+                    {n=G.UIT.R, config = {align = 'cm', maxw = 1}, nodes={
+                        {n=G.UIT.O, config={align = 'cm', object = 
+                            DynaText({scale = 0.6,
+                                string = {{ref_table = WLT.MAGIC, ref_value = 'cur_soul'}},
+                                colours = { G.C.WHITE }
+                            })
+                        }}
+                    }}
+                }},
+                config = {
+                    align = 'cm',
+                    offset = { x = 0, y = 0.18 },
+                    major = G.mana_ui:get_UIE_by_ID('soul_drop')
                 }
             }
 end
