df58ccd237a3b55bd4aa2970654c99112b623f75
thagorn <thagorn1@gmail.com>
2026-07-14 21:03:17 -0400

    aethereal potentially finished, some file cleanup

--- assets/shaders/aethereal.fs
+++ assets/shaders/aethereal.fs
@@ -99,26 +99,42 @@ vec4 effect( vec4 colour, Image texture, vec2 texture_coords, vec2 screen_coords
 {
     vec4 tex = Texel( texture, texture_coords);
     vec2 uv = (((texture_coords)*(image_details)) - texture_details.xy*texture_details.ba)/texture_details.ba;
-    vec2 adjusted_uv = uv - vec2(0.5, 0.5);
-    adjusted_uv.x = adjusted_uv.x*texture_details.b/texture_details.a;
 
-    number low = min(tex.r, min(tex.g, tex.b));
-    number high = max(tex.r, max(tex.g, tex.b));
-	number delta = min(high, max(0.5, 1. - low));
+    float minOpacity = (sin(aethereal.y * .5) - .3) * .2;
+    float maxOpacity = minOpacity + .2;
+    // rotating horizontal bands
+    float fac1 = 1-(max(minOpacity, min(distance(uv.y, (aethereal.r)), maxOpacity)));
+    float fac2 = 1-(max(minOpacity, min(distance(uv.y, (aethereal.r - 1)), maxOpacity)));
+    float maxfac = max(fac1, fac2);
+    
+    float low = min(tex.r, min(tex.g, tex.b));
+    float high = max(tex.r, max(tex.g, tex.b));
+	float delta = min(high, max(.5, 1. - low));
 
-    number fac = max(min(2.*sin((length(90.*adjusted_uv) + aethereal.r*2.) + 3.*(1.+0.8*cos(length(113.1121*adjusted_uv) - aethereal.r*3.121))) - 1. - max(5.-length(90.*adjusted_uv), 0.), 1.), 0.);
-    vec2 rotater = vec2(cos(aethereal.r*0.1221), sin(aethereal.r*0.3512));
-    number angle = dot(rotater, adjusted_uv)/(length(rotater)*length(adjusted_uv));
-    number fac2 = max(min(5.*cos(aethereal.g*0.3 + angle*3.14*(2.2+0.9*sin(aethereal.r*1.65 + 0.2*aethereal.g))) - 4. - max(2.-length(20.*adjusted_uv), 0.), 1.), 0.);
-    number fac3 = 0.3*max(min(2.*sin(aethereal.r*5. + uv.x*3. + 3.*(1.+0.5*cos(aethereal.r*7.))) - 1., 1.), -1.);
-    number fac4 = 0.3*max(min(2.*sin(aethereal.r*6.66 + uv.y*3.8 + 3.*(1.+0.5*cos(aethereal.r*3.414))) - 1., 1.), -1.);
-
-    number maxfac = max(max(fac, max(fac2, max(fac3, max(fac4, 0.0)))) + 2.2*(fac+fac2+fac3+fac4), 0.);
-
-    tex.r = tex.r-delta + delta*maxfac*0.3;
-    tex.g = tex.g-delta + delta*maxfac*0.3;
+    // lower alpha and raise white to compensate
+    tex.r = tex.r-delta + delta*maxfac*.3;
+    tex.g = tex.g-delta + delta*maxfac*.3;
     tex.b = tex.b + delta*maxfac*1.9;
-    tex.a = min(tex.a, 0.3*tex.a + 0.9*min(0.5, maxfac*0.1));
+    tex.a = (1.-maxfac) * tex.a;
+
+    float center_distance = .3 - distance(uv, vec2(.5, .5));
+    if (center_distance > 0.)
+    {
+        // purple core
+        tex.r = (1. - tex.r) * center_distance * .5 + tex.r;
+        tex.g = tex.g - ((tex.g - 0.) * center_distance);
+        tex.b = (1. - tex.b) * center_distance * .5 + tex.b;
+    }
+    float edge_distance = .2 - min(min(min(distance(uv.x, 0.), distance(uv.x, 1.)), distance(uv.y, 0.)), distance(uv.y, 1.));
+    float adj_ed = (edge_distance + (center_distance * -1.))/2.;
+    if (adj_ed > 0.)
+    {
+        // yellow glow
+        adj_ed *= 1.25;
+        tex.r = (1. - tex.r) * adj_ed + tex.r;
+        tex.g = (1. - tex.g) * adj_ed + tex.g;
+        tex.b = tex.b - ((tex.b - 0.) * adj_ed * 2.);
+    }
 
     return dissolve_mask(tex, texture_coords, uv);
 }
