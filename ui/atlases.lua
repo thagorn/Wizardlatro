bb783200ac256d18291e4a334062fec6e90f3bec
thagorn <thagorn1@gmail.com>
2026-08-06 20:46:14 -0400

    Octarine suit base layer and ui icon

--- ui/atlases.lua
+++ ui/atlases.lua
@@ -25,3 +25,17 @@ SMODS.Atlas({
     px = 71,
     py = 95
 })
+
+SMODS.Atlas({
+    key = "suits",
+    path = "suits.png",
+    px = 71,
+    py = 95
+})
+
+SMODS.Atlas({
+    key = "suits_ui",
+    path = "suits_ui.png",
+    px = 18,
+    py = 18
+})
