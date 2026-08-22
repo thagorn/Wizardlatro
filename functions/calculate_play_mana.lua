e93cc17bca58e06c78f94f5e13b9451ccbcf8a95
thagorn <thagorn1@gmail.com>
2026-08-22 06:47:32 -0400

    improve mana calc logic

--- functions/calculate_play_mana.lua
+++ functions/calculate_play_mana.lua
@@ -29,8 +29,7 @@ WLT.calculate_play_mana_card = function(card, scoring_hand)
             for key, eval2 in pairs(eval) do
                 if key == 'free' or (type(eval2) == 'table' and eval2.free) then
                     mana = mana * 0
-                end
-                if key == 'invert' or (type(eval2) == 'table' and eval2.invert) then
+                elseif key == 'invert' or (type(eval2) == 'table' and eval2.invert) then
                     mana = mana * -1
                 end
             end
